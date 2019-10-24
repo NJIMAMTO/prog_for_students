a066fd23bfd28ed9e2705ab7a6e3bd29dcfa5bd1
NJIMAMTO <s17018@g.metro-cit.ac.jp>
2019-10-24 23:22:59 +0900

    宿題03を追加

---
+++ 2_python/2_3_1.py
@@ -0,0 +1,38 @@
+# programing school 03スライドの宿題結果 
+ 
+# 1.代数演算子を使って計算
+a = 10
+b = 3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+# 2.代入演算子を使って計算
+a = 3
+b = 12
+
+a += b
+print(a)
+
+a -= b
+print(a)
+
+a *= b
+print(a)
+
+a /= b
+print(a)
+
+a %= b
+print(a)
+
+a **= b
+print(a)
+
+a //= b
+print(a)
