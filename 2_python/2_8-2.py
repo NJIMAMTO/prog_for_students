1f34074c72c2a37c324c90d028bc24b74a6b290e
NJIMAMTO <s17018@g.metro-cit.ac.jp>
2019-11-05 17:50:48 +0900

    5枚目に沿うプログラムを追加

---
+++ 2_python/2_8-2.py
@@ -0,0 +1,8 @@
+#for文の例＃
+#プログラミング教室05に遵守#
+
+list = ["a","b","c"]
+
+#listのからa,b,cをひとつずつ取り出す
+for a in list:
+    print(a)
---
+++ 2_python/5th_paper_homework.py
@@ -0,0 +1,29 @@
+#プログラミング教室05の課題#
+#a+bの結果が100以上ならtrue 未満ならfalseを返す#
+#python3.8以降で使えるセイウチ演算子を使った行数圧縮バージョン#
+
+if((a:=int(input())) + (b:=int(input())) > 100):
+    print("True")
+else:
+    print("False")
+
+#普通に書いたバージョン
+
+print("aを入力")
+a = int(input())
+print("bを入力")
+b = int(input())
+
+if (a+b) > 100:
+    print("True")
+else:
+    print("False")    
+
+
+#0~100までの偶数を全て足す#
+b = 0
+for a in range(1,101):
+    if a%2 == 0:
+        b = b + a
+
+print(b)
