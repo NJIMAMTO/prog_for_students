2d5fda48a430c213cecc0542a25d8344db234f66
NJIMAMTO <kinntetunetwork@gmail.com>
2019-11-29 16:16:58 +0900

    宿題ヒントのミスを修正

--- 3_python/homework_hint.py
+++ 3_python/homework_hint.py
@@ -1,10 +1,16 @@
+##
+##マイクラのテキストに任意の2つの文章を表示させるプログラム
+##
 import sys #コマンドライン引数を使うために必要なモジュール
 from mcpi.minecraft import Minecraft #Minecraftクラスの呼び出し
 
 args = sys.argv #コマンドラインに入力された引数を受け取る
 
-arg1 = args[0]
-arg2 = args[1]
+#argsをひとつずつに分割する
+args1 = args[1]
+args2 = args[2]
+
 mc = Minecraft.create() #Minecraftとの接続を作成
+
 mc.postToChat(args1) #コマンドライン引数をチャットに表示
 mc.postToChat(args2) #コマンドライン引数をチャットに表示
