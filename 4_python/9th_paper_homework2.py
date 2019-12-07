df92a9ce49c32ef22173ff351e5128fbb6d7129e
NJIMAMTO <kinntetunetwork@gmail.com>
2019-12-07 12:39:26 +0900

    宿題を修正

--- 4_python/9th_paper_homework2.py
+++ 4_python/9th_paper_homework2.py
@@ -1,5 +1,5 @@
 ##
-##サンプルプログラムloop4.pyをwhilw文で書き直したプログラム
+##逆さまのピラミッドをつくるプログラム
 ##
 from mcpi.minecraft import Minecraft #Minecraftクラスの呼び出し
 from mcpi.block import * 
@@ -9,7 +9,8 @@ mc = Minecraft.create() #Minecraftとの接続を作成
 pos = mc.player.getTilePos() #自分のいる位置を取得
 #そこを基準にしてピラミッドを作成
 height = 51 #ピラミッドの高さ
-length = height*2-1 #一番下のブロックの数(正方形の一辺の長さ)
+length = height*2-1 #一番上のブロックの数(正方形の一辺の長さ)
+#上からピラミッドを作っていく
 for i in range(height):
     mc.setBlocks(pos.x+i,pos.y+(height-i),pos.z+i,pos.x+(length-i+1),pos.y+(height-i),pos.z+(length-i+1),STONE)
 
